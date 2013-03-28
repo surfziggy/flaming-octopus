c1249000af391a2a0d3d64669018a70ca5e457fd
unknown <jsamways@NDSAUS-JSAMWAYS.au.uk.nds.com>
2013-03-28 11:11:42 +1100

    General tidy up and refactor Addition of HUD Sprites :work in progress

---
+++ LevelLibrary/GameObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LevelLibrary
+{
+    public class GameObject
+    {
+        public Vector2 position;                                // Player coords relative to top left screen
+        public virtual int Height { get; set; }  
+        public virtual int Width { get; set; }
+        public virtual int Health { get; set; }
+        public virtual int Lives { get; set; }
+        public virtual int Rotation { get; set; }
+
+        public GameObject()
+        {
+            Health = 100;
+            Lives = 3;
+        }
+
+        public virtual void Hit(int hitPower)
+        {
+            Health -= hitPower;
+            if (Health < 0)
+            {
+                Lives--;
+                Health = 100;
+            }
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Rectangle GetBounds()
+        {
+            return (new Rectangle((int)position.X, (int)position.Y, Width, Height));
+        }
+    }
+}
---
+++ Shooter/Shooter/HUDisplay.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MyPlat
+{
+    class HUDisplay
+    {
+        Texture2D lifeBar;
+        Texture2D livesIcon;
+        SpriteFont font;
+        int health;
+        int lives;
+        
+        /// <summary>
+        /// Initialise the Heads Up Display
+        /// </summary>
+        public void Initialize(SpriteFont hudFont )
+        {
+            health = 0;
+            lives = 0;
+            font = hudFont;
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        public void LoadContent(ContentManager Content)
+        {
+            lifeBar = Content.Load<Texture2D>("GenericTextures\\healthBar");
+            livesIcon = Content.Load<Texture2D>("GenericTextures\\livesIcon");
+        }
+                /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        public void UnloadContent(ContentManager Content)
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+       public void Update(GameTime gameTime, LevelLibrary.GameObject player)
+        {
+            health = player.Health;
+            lives = player.Lives;
+        }
+
+        
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void Draw(GameTime gameTime, SpriteBatch hudBatch)
+        {
+            // Game info spritebatch
+            hudBatch.Begin();
+
+            // Draw the life bar
+            hudBatch.Draw(lifeBar, new Rectangle( 50, 40, health, 9),  Color.White);
+
+            // Draw an icon for each life left, 9x9 at a spacing of 20px horizontally
+            for (int life = 0; life < lives; life++ )
+            {
+                hudBatch.Draw(livesIcon, new Rectangle((50 + (life * 20)), 50, 9, 9), Color.White);
+            }
+
+            hudBatch.End();
+        }
+    }
+}
